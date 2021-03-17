250058a7d01c5cbad0aec53b816eacf12310fb71
Emely Cruz <70857497+00197219@users.noreply.github.com>
2021-03-16 21:17:07 -0600

    Update Program.cs

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -14,13 +14,13 @@ namespace Example1
             Console.WriteLine("Enter a value for b:");
             b = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)} ");
+            Console.WriteLine($"Adding {a} * {b} equals {AddNumbers(a, b)} ");
         }
 
         public static int AddNumbers(int a, int b)
         {
 
-            return a - b;
+            return a * b;
         }
     }
 }
